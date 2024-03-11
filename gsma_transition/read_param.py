bb174a04f83125efdef1c4ac8897590478a5cfcc
Zane <yoruichi2018@outlook.com>
2024-03-11 17:27:03 +0800

    Update read_param.py
    minor improvements

--- gsma_transition/read_param.py
+++ gsma_transition/read_param.py
@@ -2,6 +2,7 @@ import os
 import sys
 import re
 import platform
+import argparse
 
 #check running OS type, return 'Linux' or 'Windows' or 'UNIX'
 def check_os():
@@ -60,119 +61,81 @@ def read_param_files(directory_path):
     print(file_contents_dict)
     return file_contents_dict
 
-def append_to_zabbix_confition_file(config_file, content):
+def append_to_zabbix_configration_file(config_file, content):
     scripts_dir = check_dir()
     file_path = os.path.join(scripts_dir, config_file)
     with open(file_path, 'a') as file:
         file.write(content + '\n')
 
-def convert_gsma_param(directory_path):
-    file_contents_dict = read_param_files(directory_path)
-    #LogMonitor.param and LogMonitor_detail.param processing
+def process_log_monitor_params(gsma_params_contents_dict):
+    logMonitor_contents_lists = gsma_params_contents_dict.get("LogMonitor.param")
+    LogMonitor_detail_contents_lists = gsma_params_contents_dict.get("LogMonitor_detail.param")
     files_dict = {}
-    zbx_contents = ""
-    logMonitor_contents_lists = file_contents_dict.get("LogMonitor.param")
-    LogMonitor_detail_contents_lists = file_contents_dict.get("LogMonitor_detail.param")
-    if logMonitor_contents_lists is not None:
-        for file_defines in logMonitor_contents_lists:
-            elements = file_defines.split(";")
-            file_alias = elements[0]
-            file_path = elements[1]
-            files_dict[file_alias] = file_path
-        #replace file alias in LogMonitor_detail.param with file path
-        for detail_line in LogMonitor_detail_contents_lists:
-            final_result = []
-            elements = detail_line.split(";")
-            file_tag = elements[0]
-            file_alias = elements[1]
-            file_keyword = elements[2]
-            file_severity = elements[3]
-            file_full_path = files_dict.get(file_alias)
-            file_dir, file_name = file_full_path.rsplit(os.sep, 1)
-            final_result.append(file_tag)
-            final_result.append(file_dir)
-            final_result.append(file_name)
-            final_result.append(file_keyword)
-            final_result.append(file_severity)
-            zbx_contents = ";".join(final_result)
-            
-            save_params_to_zabbix("zbx_logMonitor.conf", zbx_contents)
-            save_params_to_zabbix("zbx_logMonitor.conf", '\n')
+    for file_defines in logMonitor_contents_lists:
+        elements = file_defines.split(";")
+        file_alias, file_path = elements[0], elements[1]
+        files_dict[file_alias] = file_path
+    #replace file alias in LogMonitor_detail.param with file path
+    for detail_line in LogMonitor_detail_contents_lists:
+        elements = detail_line.split(";")
+        file_tag, file_alias, file_keyword, file_severity = elements[0], elements[1], elements[2], elements[3]
+        file_full_path = files_dict.get(file_alias)
+        file_dir, file_name = file_full_path.rsplit(os.sep, 1)
+        final_result = ";".join([file_tag, file_dir, file_name, file_keyword, file_severity])
+        append_to_zabbix_configration_file("zbx_logMonitor.conf", final_result)
 
-    #process.param processing
-    process_contents_lists = file_contents_dict.get("process.param")
-    if process_contents_lists is not None:
-        for items in process_contents_lists:
-            elements = items.split(";")
-            final_result = []
-            process_tag = elements[0] + "_" + elements[1]
-            process_name = elements[2]
-            process_user = elements[3]
-            process_count = elements[4]
-            process_severity = elements[7]
-            if process_severity == "W" or process_severity == "M":
-                process_severity = "WARNING"
-            elif process_severity == "C":
-                process_severity = "CRITICAL"
-            elif process_severity == "F":
-                process_severity = "FATAL"
-            final_result.append(process_tag)
-            final_result.append(process_name)
-            final_result.append(process_user)
-            final_result.append(process_count)
-            final_result.append(process_severity) 
-            zbx_contents = ";".join(final_result)
-            save_params_to_zabbix("zbx_processMonitor.conf", zbx_contents)
-            save_params_to_zabbix("zbx_processMonitor.conf", '\n')
+def process_process_params(gsma_params_contents_dict):
+    process_contents_lists = gsma_params_contents_dict.get("process.param")
+    for items in process_contents_lists:
+        elements = items.split(";")
+        process_tag, process_name, process_user, process_count, process_severity = elements[0] + "_" + elements[1], elements[2], elements[3], elements[4], elements[7]
+        if process_severity == "W" or process_severity == "M":
+            process_severity = "WARNING"
+        elif process_severity == "C":
+            process_severity = "CRITICAL"
+        elif process_severity == "F":
+            process_severity = "FATAL"
+        final_result = ";".join([process_tag, process_name, process_user, process_count, process_severity])
+        append_to_zabbix_configration_file("zbx_processMonitor.conf", final_result)
 
-    #networkport.param processing
-    network_contents_lists = file_contents_dict.get("networkport.param")
-    if network_contents_lists is not None:
-        for items in network_contents_lists:
-            elements = items.split(";")
-            final_result = []
-            network_tag = elements[0] + "_" + elements[1]
-            network_hostname = elements[2]
-            network_port = elements[3]
-            network_severity = elements[4]
-            final_result.append(network_tag)
-            final_result.append(network_hostname)
-            final_result.append(network_port)
-            final_result.append(network_severity)
-            zbx_contents = ";".join(final_result)
-            save_params_to_zabbix("zbx_networkMonitor.conf", zbx_contents)
-            save_params_to_zabbix("zbx_networkMonitor.conf", '\n')
+def process_network_port_params(gsma_params_contents_dict):
+    network_contents_lists = gsma_params_contents_dict.get("networkport.param")
+    for items in network_contents_lists:
+        elements = items.split(";")
+        network_tag, network_hostname, network_port, network_severity = elements[0] + "_" + elements[1], elements[2], elements[3], elements[4]
+        final_result = ";".join([network_tag, network_hostname, network_port, network_severity])
+        append_to_zabbix_configration_file("zbx_networkMonitor.conf", final_result)
 
-    #EventLog.param processing
-    #gsma format
-    #MonSol; Logfile; Type/Level; Source; EventCode; User; Severity
-    eventlog_contents_lists = file_contents_dict.get("EventLog.param")
-    if eventlog_contents_lists is not None:
-        for item in eventlog_contents_lists:
-            elements = item.split(";")
-            final_result = []
-            eventlog_tag = elements[0]
-            eventlog_logfile = elements[1].replace("=", "")
-            eventlog_keyword = ""
-            eventlog_level = elements[2]
-            eventlog_source = elements[3].replace("=","")
-            eventlog_id = elements[4].replace("=", "")
-            eventlog_severity = elements[6]
-            final_result.append(eventlog_tag)
-            final_result.append(eventlog_logfile)
-            final_result.append(eventlog_keyword)
-            final_result.append(eventlog_level)
-            final_result.append(eventlog_source)
-            final_result.append(eventlog_id)
-            final_result.append(eventlog_severity)
-            zbx_contents = ";".join(final_result)
-            save_params_to_zabbix("zbx_eventLogMonitor.conf", zbx_contents)
-            save_params_to_zabbix("zbx_eventLogMonitor.conf", '\n')
+def process_event_log_params(gsma_params_contents_dict):
+    eventlog_contents_lists = gsma_params_contents_dict.get("EventLog.param")
+    for item in eventlog_contents_lists:
+        elements = item.split(";")
+        eventlog_tag, eventlog_logfile, eventlog_keyword, eventlog_level, eventlog_source, eventlog_id, eventlog_severity = elements[0], elements[1].replace("=", ""), "", elements[2], elements[3].replace("=",""), elements[4].replace("=", ""), elements[6]
+        final_result = ";".join([eventlog_tag, eventlog_logfile, eventlog_keyword, eventlog_level, eventlog_source, eventlog_id, eventlog_severity])
+        append_to_zabbix_configration_file("zbx_eventLogMonitor.conf", final_result)
+
+def convert_gsma_param(directory_path):
+    gsma_params_contents_dict = read_param_files(directory_path)
+    #process each parameter type
+    if "LogMonitor.param" in gsma_params_contents_dict and "LogMonitor_detail.param" in gsma_params_contents_dict:
+        process_log_monitor_params(gsma_params_contents_dict)
+    
+    if "process.param" in gsma_params_contents_dict:
+        process_process_params(gsma_params_contents_dict)
+
+    if "networkport.param" in gsma_params_contents_dict:
+        process_network_port_params(gsma_params_contents_dict)
+    
+    if "EventLog.param" in gsma_params_contents_dict:
+        process_event_log_params(gsma_params_contents_dict)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Process GSMA parameters and save to Zabbix configuration.")
+    parser.add_argument("directory_path", help="Path to the directory containing .param files")
+    return parser.parse_args()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <directory_path>")
-    else:
-        directory_path = sys.argv[1]
-        #print(read_param_files(directory_path))
-        convert_gsma_param(directory_path)
+    args = parse_arguments()
+    directory_path = args.directory_path
+    convert_gsma_param(directory_path)
