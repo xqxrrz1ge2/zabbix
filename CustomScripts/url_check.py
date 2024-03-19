c902b7c9c3df958483460b4e2380f3fe90ad82a3
Zane <zhazhahui_cn@outlook.com>
2024-03-19 23:40:31 +0800

    remove comments

--- CustomScripts/url_check.py
+++ CustomScripts/url_check.py
@@ -3,22 +3,18 @@ from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
 
 def check_url_status(url):
-    # 检查URL是否以http://或https://开头
     if not (url.startswith("http://") or url.startswith("https://")):
         print("Error: URL should start with 'http://' or 'https://'")
         return
     try:
         response = urlopen(url)
-        # 如果状态码是200，打印0；否则打印1
         if response.status == 200:
             print(0)
         else:
             print(1)
     except HTTPError as e:
-        # 如果服务器能够对请求作出响应，但是响应的是一个错误代码，打印1
         print(1)
     except URLError as e:
-        # 如果我们无法达到服务器（比如URL错误，或网络问题），打印1
         print(1)
 
 if __name__ == "__main__":
--- CustomScripts/url_check_python2.py
+++ CustomScripts/url_check_python2.py
@@ -2,23 +2,19 @@ import sys
 import urllib2
 
 def check_url_status(url):
-    # 检查URL是否以http://或https://开头
     if not (url.startswith("http://") or url.startswith("https://")):
         print "Error: URL should start with 'http://' or 'https://'"
         return
     
     try:
         response = urllib2.urlopen(url)
-        # 如果状态码是200，打印0；否则打印1
         if response.getcode() == 200:
             print 0
         else:
             print 1
     except urllib2.HTTPError as e:
-        # 如果服务器能够对请求作出响应，但是响应的是一个错误代码，打印1
         print 1
     except urllib2.URLError as e:
-        # 如果我们无法达到服务器（比如URL错误，或网络问题），打印1
         print 1
 
 if __name__ == "__main__":
